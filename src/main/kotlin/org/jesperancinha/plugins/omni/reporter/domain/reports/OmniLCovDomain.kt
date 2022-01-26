8a08cec3ea224f01b34cf9b31ba4c13c61372be9
Joao Esperancinha <jofisaes@gmail.com>
2022-01-27 00:00:55 +0100

    👾 LCov model data

--- src/main/kotlin/org/jesperancinha/plugins/omni/reporter/domain/reports/OmniLCovDomain.kt
+++ src/main/kotlin/org/jesperancinha/plugins/omni/reporter/domain/reports/OmniLCovDomain.kt
@@ -1,7 +1,89 @@
 package org.jesperancinha.plugins.omni.reporter.domain.reports
 
+
+data class LineData(
+    /**
+     * Line Number
+     */
+    val lineNumber: Int,
+    /**
+     * Hit Count
+     */
+    val hitCount: Int
+)
+
+data class Function(
+    /**
+     * Line Number
+     */
+    val line: Int,
+    /**
+     * Function Name
+     */
+    val name: String
+)
+
+data class BranchData(
+    /**
+     * Line Number
+     */
+    val line: Int,
+    /**
+     * Block Number
+     */
+    val block: Int,
+    /**
+     * Number of Expressions
+     */
+    val expressions: Int,
+    /**
+     * Blocck Count
+     */
+    val count: Int
+)
+
 /**
  * Created by jofisaes on 26/01/2022
  */
-class OmniLCovDomain {
-}
+data class OmniLCovReport(
+    /**
+     * TN
+     */
+    val testName: String,
+    /**
+     * SF
+     */
+    val sourceFilePath: String,
+    /**
+     * FN
+     */
+    val functionName: Function,
+    /**
+     * FNF
+     */
+    val functionNumberFound: Int,
+    /**
+     * FNH
+     */
+    val functionNumberHits: Int,
+    /**
+     * BRDA
+     */
+    val branchData: BranchData,
+    /**
+     * BRF
+     */
+    val branchFound: Int,
+    /**
+     * DA
+     */
+    val data: LineData,
+    /**
+     * LF
+     */
+    val linesFound: Int,
+    /**
+     * LH
+     */
+    val linesHit: Int
+)
