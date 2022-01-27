6eb39f852c663ad011b7c33a05a24ff68283360a
Joao Esperancinha <jofisaes@gmail.com>
2022-01-27 14:17:42 +0100

    👾 Preparation for Clover Reports

--- src/main/kotlin/org/jesperancinha/plugins/omni/reporter/domain/reports/OmniCloverDomain.kt
+++ src/main/kotlin/org/jesperancinha/plugins/omni/reporter/domain/reports/OmniCloverDomain.kt
@@ -1,7 +1,36 @@
 package org.jesperancinha.plugins.omni.reporter.domain.reports
 
+import com.fasterxml.jackson.annotation.JsonRootName
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+
+data class CloverFile(
+    val name: String,
+    val path: String
+)
+
+data class Metrics(
+    val statements: Int,
+    val coveredstatements: Int,
+    val conditionals: Int,
+    val coveredconditionals: Int,
+    val methods: Int,
+    val coveredmethods: Int
+)
+
+data class Project(
+    val timestamp: String,
+    val name: String,
+    val metrics: Metrics,
+    val file: CloverFile,
+)
+
 /**
  * Created by jofisaes on 26/01/2022
  */
-class OmniCloverReport {
-}
+@JsonRootName("coverage")
+@JacksonXmlRootElement
+data class OmniCloverReport(
+    val generated: String,
+    val clover: String,
+    val project: Project
+)
--- src/test/resources/src/app/App.tsx
+++ src/test/resources/src/app/App.tsx
@@ -1,4 +1,6 @@
+// @ts-ignore
 import React from 'react';
+// @ts-ignore
 import logo from './logo.svg';
 import './App.css';
 
